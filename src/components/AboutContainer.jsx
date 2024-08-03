eeb2cd6ea6b32bc73f12b8055204aadc4bcc5caa
Sr Chris <rodrigochristianxz@hotmail.com>
2024-08-03 16:46:55 -0300

    Add files via upload

---
+++ src/components/AboutContainer.jsx
@@ -0,0 +1,21 @@
+const AboutContainer = () => {
+  return (
+    <section className="about-container">
+      <h2>Sobre</h2>
+      <p>
+      Olá! Eu sou Rodrigo Christian, um desenvolvedor em formação apaixonado por tecnologia. Já 
+      desenvolvi projetos pessoais, como a criação de um organizador de pastas, um aplicativo de lista de 
+      tarefas inteligente e um sistema de gerenciamento de inventário para pequenas empresas.
+      </p>
+      <p>
+      Tenho um grande interesse em design moderno e futurista, aplicando princípios de design para criar 
+      interfaces atraentes e funcionais. Estou focado em fazer o meu melhor para você ou sua empresa.
+      <br></br>
+      <br></br>
+      Se você está procurando um desenvolvedor criativo para seu próximo projeto, adoraria conversar!
+      </p>
+    </section>
+  );
+};
+
+export default AboutContainer;
